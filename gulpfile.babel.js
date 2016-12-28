1137cf0223cec816de784ec7eb1256db66a1ca44
dstyle <dstyle0210@gmail.com>
2016-12-28 11:55:29 +0900

    init

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -18,19 +18,13 @@ gulp.task("yeoman",() => {
         gutil.log("workspace setting success.");
 
         gutil.log("request HTML Template (https://dstyle0210.github.io/dsPack/template.html).");
-        var template = '<!doctype html>\n' +
-            '<html lang="ko">\n' +
-            '<head>\n' +
-            '\t<meta charset="UTF-8">\n\t<meta http-equiv="x-ua-compatible" content="ie=edge">\n\t<meta name="viewport" content="width=device-width, initial-scale=1">\n' +
-            '\t<title></title>\n' +
-            '\t<link rel="stylesheet" href="./css/styles.css">\n' +
-            '</head>\n' +
-            '<body>\n\n' +
-            '</body>\n' +
-            '</html>';
-        fs.writeFile('src/template.html', template, 'utf8', (err)=>{
-            if(err) console.log(err);
+        request({
+            url:"https://dstyle0210.github.io/dsPack/template/html5.html"
+        },function(err,res,html){
+            fs.writeFile('src/template.html', html, 'utf8', (err)=>{
+                if(err) console.log(err);
+            });
+            gutil.log("Make HTML Template End.");
         });
-        gutil.log("Make HTML Template End.");
     });
 });
