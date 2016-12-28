17cf8baec0b7c47d921aefbe6a32c439b758df78
dstyle <dstyle0210@gmail.com>
2016-12-28 15:08:03 +0900

    init

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,13 +5,19 @@ import gulp from "gulp";
 import gutil from "gulp-util";
 import mkdirp from "mkdirp";
 import request from "request";
+import less from "gulp-less";
+import scss from "gulp-sass";
 
 const templates = {
     html5 : "https://dstyle0210.github.io/dsPack/template/html5.html",
     htmlxt: "https://dstyle0210.github.io/dsPack/template/xhtml.html"
 }
+var savePromise = [];
 
 gulp.task("default",() => {
+
+});
+gulp.task("yeoman",["yeoman:html5"],() => {
 
 });
 gulp.task("yeoman:html5",() => {
@@ -22,6 +28,7 @@ gulp.task("yeoman:html5",() => {
         mkdirp("src/resources/css/less/sample");
         mkdirp("src/resources/css/scss");
         mkdirp("src/resources/css/scss/_lib");
+        mkdirp("src/resources/css/scss/sample");
         mkdirp("src/resources/js/app");
         gutil.log("workspace setting success.");
 
@@ -34,13 +41,19 @@ gulp.task("yeoman:html5",() => {
             gutil.log("Make HTML Template End.");
         });
 
-        save("reset.css","src/resources/css/");
-        save("val.less","src/resources/css/less/_lib");
-        save("mixin.less","src/resources/css/less/_lib");
-        save("sample.less","src/resources/css/less/sample");
-        save("val.scss","src/resources/css/scss/_lib");
-        save("mixin.scss","src/resources/css/scss/_lib");
-        save("sample.scss","src/resources/css/scss/sample");
+        savePromise.push( save("reset.css","src/resources/css/") );
+        savePromise.push( save("val.less","src/resources/css/less/_lib") );
+        savePromise.push( save("mixin.less","src/resources/css/less/_lib") );
+        savePromise.push( save("sample-less.less","src/resources/css/less/sample") );
+        savePromise.push( save("val.scss","src/resources/css/scss/_lib") );
+        savePromise.push( save("mixin.scss","src/resources/css/scss/_lib") );
+        savePromise.push( save("sample-scss.scss","src/resources/css/scss/sample") );
+
+        Promise.all(savePromise).then(function (values) {
+            gulp.src(["src/resources/css/less/**/*.less","!src/resources/css/less/_lib/*.less"])
+                .pipe(less())
+                .pipe(gulp.dest("./src/resources/css"));
+        });
     });
 });
 
@@ -48,11 +61,14 @@ gulp.task("yeoman:html5",() => {
 function save(file,path){
     var url = "https://dstyle0210.github.io/dsPack/template/"+file;
     var path = path+"/"+file;
-    request({
-        url:url
-    },function(err,res,data){
-        fs.writeFile(path , data, 'utf8', (err)=>{
-            if(err) console.log(err);
+    return new Promise(function (resolve, reject) {
+        request({
+            url:url
+        },function(err,res,data){
+            fs.writeFile(path , data, 'utf8', (err)=>{
+                if(err) console.log(err);
+                resolve(data);
+            });
         });
     });
 };
